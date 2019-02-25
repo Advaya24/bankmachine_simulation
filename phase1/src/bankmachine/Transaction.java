2a21a7187fa74f958b7e69cce55932ae4c11e61b
Varun Lodaya <varunblodaya@gmail.com>
2019-02-25 16:32:54 -0500

    Added annotations for all attributes

--- phase1/src/bankmachine/Transaction.java
+++ phase1/src/bankmachine/Transaction.java
@@ -2,10 +2,16 @@ package bankmachine;
 import java.util.Date;
 /** Represents a Transaction within the system.
  **/
+// Person working on this: Varun
 public class Transaction {
+    /**The amount of money involve in this transaction**/
     float amount;
+    /**The account this transaction was made from**/
     Account transaction_made_from;
+    /**The account this transaction was made to**/
     Account transaction_made_to;
+    /**The Date this transaction was made**/
     Date transaction_date;
+    /**The type of transaction made**/
     transaction_types transaction_type;
 }
