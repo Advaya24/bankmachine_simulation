3f7886f0d5d2c7b07af8babef815858c16cf9c51
Janice <janicejuanning.li@mail.utoronto.ca>
2019-02-25 15:58:38 -0500

    Made classes

--- phase1/src/BankMachine/Account.java
+++ phase1/src/BankMachine/Account.java
@@ -1,4 +1,16 @@
 package BankMachine;
 
-public class Account {
+/**
+ * An account
+ */
+public abstract class Account {
+    protected float balance;
+    boolean transferIn(Account other, float amount){
+        boolean status = other.transferOut(this, amount);
+        if (status){
+            this.balance += amount;
+        }
+        return status;
+    }
+    abstract boolean transferOut(Account other, float amount);
 }
