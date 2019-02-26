e85adc6967fb20a3b795963adfa4d80cbb70ea8e
Varun Lodaya <varunblodaya@gmail.com>
2019-02-26 12:09:13 -0500

    Added Getters and Setters for all attributes, along with a Constructor. Additionally, made all attributes private.

--- phase1/src/bankmachine/User.java
+++ phase1/src/bankmachine/User.java
@@ -6,15 +6,68 @@ import java.util.ArrayList;
 // Person working on this: Varun
 public class User {
     /**Name of this User**/
-    String name;
+    private String name;
     /**All the accounts this User has**/
-    ArrayList<Account> users_accounts;
+    private ArrayList<Account> users_accounts;
     /**Primary email address of this User**/
-    String email;
+    private String email;
     /**This User's Phone Number**/
-    String phone_number;
+    private String phone_number;
     /**Username of this User, used for authentication**/
-    String username;
+    private String username;
     /**Password of this User, used for authentication**/
-    String password;
+    private String password;
+
+    public User(String name, String email, String phone_number, String username, String default_password){
+        this.name = name;
+        //TODO: Should it be ArrayList<Account> or ArrayList<>?
+        users_accounts = new ArrayList<>();
+        this.email = email;
+        this.phone_number = phone_number;
+        this.username = username;
+        this.password = default_password;
+    }
+
+    /**All the getters for this class' private variables:**/
+    public String getName(){
+        return name;
+    }
+    public ArrayList<Account> getUsersAccounts(){
+        return users_accounts;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public String getPhoneNumber() {
+        return phone_number;
+    }
+    public String getUsername() {
+        return username;
+    }
+    //TODO: Decide if we really want access to this
+    public String getPassword(){
+        return password;
+    }
+
+    /**All the setters for this class' private variables:**/
+    public void setName(String new_name){
+        name = new_name;
+    }
+    public void setEmail(String new_email){
+        email = new_email;
+    }
+    public void setPhoneNumber(String new_phone_number) {
+        phone_number = new_phone_number;
+    }
+    //TODO: Decide if we really want to be able to change username
+    public void setUsername(String new_username) {
+        username = new_username;
+    }
+    //TODO: Remember to ask twice!
+    /**Allows the user to change their password if necessary**/
+    public void setPassword(String new_password){
+        password = new_password;
+    }
+
+
 }
