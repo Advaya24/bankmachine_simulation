bbbf31b5da5c9d1dccc3bee0227250dc296d500d
Varun Lodaya <varunblodaya@gmail.com>
2019-02-26 11:50:20 -0500

    Added attributes to User, along with annotations.

--- phase1/src/bankmachine/User.java
+++ phase1/src/bankmachine/User.java
@@ -1,5 +1,20 @@
 package bankmachine;
 
+import java.util.ArrayList;
+
 /**A User within this system.**/
+// Person working on this: Varun
 public class User {
+    /**Name of this User**/
+    String name;
+    /**All the accounts this User has**/
+    ArrayList<Account> users_accounts;
+    /**Primary email address of this User**/
+    String email;
+    /**This User's Phone Number**/
+    String phone_number;
+    /**Username of this User, used for authentication**/
+    String username;
+    /**Password of this User, used for authentication**/
+    String password;
 }
