39670fae63ddc2f5c7e5446e3d80d6c22fad2d5a
meilechr <christopher.meile@mail.utoronto.ca>
2019-02-28 19:31:40 -0500

    Fixed overwriting issue of files, added comments.

--- phase1/src/bankmachine/FileManager/WriteFile.java
+++ phase1/src/bankmachine/FileManager/WriteFile.java
@@ -8,11 +8,9 @@ public class WriteFile implements FileManager {
 
     private File file;
 
+    // Constructor for WriteFile
     public WriteFile(String filename) {
-
-        //this.file = new File(filename);
-
-        // Creates file with "filenamae" in the FileManager package
+        // Creates file with "filename" in the FileManager package
         this.file = new File("phase1/src/bankmachine/FileManager", filename);
     }
 
@@ -20,6 +18,7 @@ public class WriteFile implements FileManager {
         this.file = null;
     }
 
+    //Getter for filename. Returns null if file is null.
     @Override
     public String getFileName() {
         String filename;
@@ -32,15 +31,17 @@ public class WriteFile implements FileManager {
         return filename;
     }
 
+    //Getter for date and time when file was lasted edited (mm/dd/yyy HH:mm:ss)
     @Override
     public String getLastUpdated() {
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
         return sdf.format(file.lastModified());
     }
 
-    public void writeData(String str) throws Exception {
-        FileOutputStream fos = new FileOutputStream(file, false);
-        fos.write(str.getBytes());
-        fos.close();
+    // Main Method for writing target data into file. Appends file instead of overwriting existing.
+    public void writeData(String data) throws Exception {
+        FileOutputStream output_stream = new FileOutputStream(file, true);
+        output_stream.write(data.getBytes());
+        output_stream.close();
     }
 }
