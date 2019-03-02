dbc88361eb8dfeb71d9f7fa1b33b43f605b6075b
Lorenzo Kogler Anele <lorenzo.anele@mail.utoronto.ca>
2019-03-02 14:29:19 -0500

    Implemented the createAccount method that creates a new account for a specified user.

--- phase1/src/bankmachine/BankManager.java
+++ phase1/src/bankmachine/BankManager.java
@@ -1,24 +1,47 @@
 package bankmachine;
 
-import bankmachine.account.Account;
+import bankmachine.account.*;
 
 import java.util.ArrayList;
 
 /**A Bank Manager within the system**/
 //Working on: Varun (if that's okay with y'all, seeing that I'm working on User and the two are kinda linked.
-    //Should this extend BankMachineUser?
 public class BankManager extends BankMachineUser {
     BankManager(String username, String password) {
         super(username, password);
     }
 
-    //TODO: Can a user have more than one account of the same type?
-//    public boolean newAccount(String username, String accountType) {
-//        ArrayList<Account> accounts = AccountManager.getAccounts(username);
-//        for (Account account1 : accounts) {
-//            if
-//        }
-//
-//    }
+    //TODO: Can a user have more than one account of the same type? (for now this is set to yes)
+
+    /**
+     * Create a new account for user of type accountType. Return true if the account was created.
+     * @param user
+     * @param accountType
+     * @return whether account creation succeeded
+     */
+    public boolean createAccount(User user, String accountType) {
+        Account account1;
+        switch (accountType) {
+            case "Chequing account":
+                account1 = new ChequingAccount();
+                break;
+            case "Credit card account":
+                account1 = new CreditCardAccount();
+                break;
+            case "Line of credit account":
+                account1 = new LineOfCreditAccount();
+                break;
+            case "Savings account":
+                account1 = new SavingsAccount();
+                break;
+            default:
+                System.out.println("Invalid account type. Please try again.");
+                return false;
+
+        }
+        user.addAccount(account1);
+        return true;
+
+    }
 
 }
