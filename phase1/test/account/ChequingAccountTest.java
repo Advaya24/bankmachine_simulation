d454f6c5ed3c0370ac00f6ec4b08dc7b4164077b
hanss314 <hanss314@gmail.com>
2019-03-02 17:56:37 -0500

    Add tests for chequing account

--- phase1/test/account/ChequingAccountTest.java
+++ phase1/test/account/ChequingAccountTest.java
@@ -1,15 +1,19 @@
 package account;
 
+import bankmachine.User;
+import bankmachine.account.Account;
 import bankmachine.account.ChequingAccount;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class ChequingAccountTest {
     private ChequingAccount account;
     public ChequingAccountTest(){
-        //this.account = new ChequingAccount();
+        User user = mock(User.class);
+        this.account = new ChequingAccount(user);
     }
     @Test
     public void testSanity() {
@@ -17,7 +21,10 @@ public class ChequingAccountTest {
     }
     @Test
     public void testTransfer() {
-
+        Account otherAccount = mock(Account.class);
+        when(otherAccount.transferOut(anyInt())).thenReturn(true);
+        boolean status = account.transferIn(otherAccount, 10.0);
+        assertTrue(status);
+        assertEquals(10.0, account.getDoubleBalance());
     }
-
 }
