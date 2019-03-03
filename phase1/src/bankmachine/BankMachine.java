b7ace2d9d161221ccb213460b40fed7410c65b4f
hanss314 <hanss314@gmail.com>
2019-03-03 10:49:31 -0500

    Create BankMachine with main method and getters

--- phase1/src/bankmachine/BankMachine.java
+++ phase1/src/bankmachine/BankMachine.java
@@ -1,22 +1,37 @@
 package bankmachine;
 
-import bankmachine.account.Account;
+import bankmachine.FileManager.TimeInfo;
 
 public class BankMachine {
     void executeEveryMonth(){
         // TODO: Increment all savings accounts
     }
     void executeEveryDay(){}
-    static BillManager billManager;
-    static ClientManager clientManager;
-    static Account accountManager;
-    // static TransactionManager transactionManagerl
+    private static BillManager billManager;
+    private static ClientManager clientManager;
+    private static AccountManager accountManager;
+    // static TransactionManager transactionManager
+    private static TimeInfo timeInfo;
 
     public static void main(String[] args){
         billManager = new BillManager();
+        clientManager = new ClientManager();
+        accountManager = new AccountManager();
+        timeInfo = new TimeInfo();
+        InputManager.mainLoop();
     }
 
     public static BillManager getBillManager() {
         return billManager;
+    }
+
+    public static ClientManager getClientManager(){
+        return clientManager;
+    }
+    public static AccountManager getAccountManager(){
+        return accountManager;
+    }
+    public static TimeInfo getTimeInfo(){
+        return timeInfo;
     }
 }
