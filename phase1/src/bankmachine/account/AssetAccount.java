c2c9d05f60d4fb8264d41112271198d2b96dace3
hanss314 <hanss314@gmail.com>
2019-03-02 19:12:59 -0500

    Fix typo

--- phase1/src/bankmachine/account/AssetAccount.java
+++ phase1/src/bankmachine/account/AssetAccount.java
@@ -5,7 +5,7 @@ import bankmachine.Client;
 import java.util.Date;
 
 public abstract class AssetAccount extends Account{
-    public AssetAccount(int balance, Client client, , Date creationDate) {
+    public AssetAccount(int balance, Client client, Date creationDate) {
         super(balance, client, creationDate);
     }
 }
