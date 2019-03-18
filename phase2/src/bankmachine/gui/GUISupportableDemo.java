c7c0e1ab7a9a56fa5b4ef4b7597be8e14e2f8cd7
Advaya24 <advaya24@yahoo.co.in>
2019-03-17 21:31:48 -0400

    Improved sample functionality of GUISupportableDemo to support wider usage.

--- phase2/src/bankmachine/gui/GUISupportableDemo.java
+++ phase2/src/bankmachine/gui/GUISupportableDemo.java
@@ -1,13 +1,20 @@
 package bankmachine.gui;
 
+
+import java.util.function.Function;
+
 public class GUISupportableDemo implements GUISupportable {
     private String selectedString;
+    private Function<Void, Void> function;
+    public GUISupportableDemo(Function<Void, Void> function) {
+        this.function = function;
+    }
     @Override
     public void buttonClicked(String s) {
         System.out.println("Selected " + s);
         System.out.println(s.getClass());
         selectedString = s;
-        Main.action();
+        function.apply(null);
     }
 
     public String getSelectedString(){
--- phase2/src/bankmachine/gui/Main.java
+++ phase2/src/bankmachine/gui/Main.java
@@ -2,17 +2,22 @@ package bankmachine.gui;
 
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.function.Function;
 
 public class Main {
     public static ArrayList<String> sampleChoices = new ArrayList<>();
-    public static GUISupportable listener = new GUISupportableDemo();
+    public static GUISupportable listener = new GUISupportableDemo(aVoid -> {
+        action();
+        return null;
+    });
+    public static JFrame frame = new JFrame("ChoiceForm");
 
     public static void main(String[] args) {
 
         for (int i = 1; i < 6; i++) {
             sampleChoices.add("Sample Choice " + i);
         }
-        JFrame frame = new JFrame("ChoiceForm");
+
         frame.setContentPane(new ChoiceForm(sampleChoices, listener).panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
@@ -21,6 +26,7 @@ public class Main {
 
     public static void action() {
         System.out.println(sampleChoices.indexOf(((GUISupportableDemo) listener).getSelectedString()));
+        frame.dispose(); // Removes frame
     }
 
 //    public void buttonClicked(){
