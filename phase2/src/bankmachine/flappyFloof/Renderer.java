88c42277b29248ddd203ad5abf91b768fa6e72e1
Varun Lodaya <varunblodaya@gmail.com>
2019-03-23 15:38:18 -0400

    Added floof stuff but needs heavy refactoring.

---
+++ phase2/src/bankmachine/flappyFloof/Renderer.java
@@ -0,0 +1,23 @@
+package bankmachine.flappyFloof;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Renderer extends JPanel {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Paints the JPanel, and our Floof.
+     * @param g //TODO: HELP
+     */
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        FlappyFloof.flappyFloof.repaint(g);
+    }
+
+    protected void extendedPaintComponent(){
+
+    }
+}
