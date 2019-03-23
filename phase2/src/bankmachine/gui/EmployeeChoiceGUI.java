1859f4e9d953f437e4097f4c6ff60a2e9b96bb25
Advaya24 <advaya24@yahoo.co.in>
2019-03-23 15:40:15 -0400

    Ported GUI to support personal use for BankEmployee instances

---
+++ phase2/src/bankmachine/gui/EmployeeChoiceGUI.java
@@ -0,0 +1,29 @@
+package bankmachine.gui;
+
+import bankmachine.users.BankEmployee;
+import bankmachine.users.BankManager;
+
+public class EmployeeChoiceGUI implements Inputtable {
+    private BankEmployee employee;
+
+    public EmployeeChoiceGUI(BankEmployee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public void handleInput(InputManager m) {
+        String[] options = {"Personal", "Bank"};
+        m.setPanel(new OptionsForm<String>(options, "Which services do you wish to access?") {
+            @Override
+            public void onSelection(String s) {
+                if (s.equals("Personal")) {
+                    new ClientGUI(employee).handleInput(m);
+                } else if (employee instanceof BankManager) {
+                    new BankManagerGUI((BankManager) employee).handleInput(m);
+                } else {
+                    // Make BankEmployeeGUI and implement here.
+                }
+            }
+        });
+    }
+}
