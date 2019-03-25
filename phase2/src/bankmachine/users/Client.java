2f6fdc76a6b1ce5eded8fd98d6fd557c12a4d8da
Lorenzo Kogler Anele <lorenzo.anele@mail.utoronto.ca>
2019-03-25 17:40:38 -0400

    Now every new client automatically gets a primary chequing account.

--- phase2/src/bankmachine/users/Client.java
+++ phase2/src/bankmachine/users/Client.java
@@ -19,9 +19,9 @@ public class Client extends BankMachineUser {
 
     public Client(int id, String name, String email, String phoneNumber, String username, String default_password) {
         super(id,name,email, phoneNumber, username, default_password);
-        AccountFactory factory = BankMachine.accFactory;
-        factory.newChequingAccount(0, this, LocalDateTime.now());
-        ((ChequingAccount)this.clientsAccounts.get(0)).setPrimary(true);
+//        AccountFactory factory = BankMachine.accFactory;
+//        factory.newChequingAccount(0, this, LocalDateTime.now());
+//        ((ChequingAccount)this.clientsAccounts.get(0)).setPrimary(true);
     }
 
     /**All the getters for this class' private variables:**/
