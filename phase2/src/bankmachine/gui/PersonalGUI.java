874db65c70fdbe05f76e65c3904446772153891f
Advaya24 <advaya24@yahoo.co.in>
2019-03-29 15:58:06 -0400

    Extracted TransferGUIHandler

--- phase2/src/bankmachine/gui/PersonalGUI.java
+++ phase2/src/bankmachine/gui/PersonalGUI.java
@@ -47,238 +47,10 @@ public class PersonalGUI implements Inputtable {
         });
     }
 
-//    private void handleFinance(InputManager m) {
-//        String[] financeOptions = {"Stocks", "Exchange", "Mortgage"};
-//
-//        m.setPanel(new SearchForm("Select a finance service", new OptionsForm<String>(financeOptions, "") {
-//            @Override
-//            public void onSelection(String s) {
-//                switch (s) {
-//                    case "Stocks":
-//                        handleStocks(m);
-//                        return;
-//                    case "Exchange":
-//                        handleExchange(m);
-//                        return;
-//                    case "Mortgage":
-//                        handleMortgage(m);
-//                }
-//            }
-//        }.getMainPanel()) {
-//            @Override
-//            public void onCancel() {
-//                handleInput(m);
-//            }
-//        });
-//    }
 
-
-
-    private void inputGetAccounts(InputManager m) {
-        JPanel panel = null;
-        String[] accountSummary = client.getAccountSummary().clone();
-        if (client.getClientsAccounts().size() > 0) {
-            panel = new OptionsForm<Object>(client.getClientsAccounts().toArray(), ""){
-                @Override
-                public void onSelection(Object obj) {
-                    handleAccount(m, (Account)obj);
-                }
-            }.getMainPanel();
-        } else {
-            String[] tempAccountSummary = new String[accountSummary.length + 1];
-            for (int i = 0; i < accountSummary.length; i++) {
-                tempAccountSummary[i] = accountSummary[i];
-            }
-            tempAccountSummary[accountSummary.length] = "No accounts to show";
-            accountSummary = tempAccountSummary;
-        }
-        m.setPanel(new AccountSummaryForm(accountSummary, panel) {
-            @Override
-            public void onCancel() {
-                handleInput(m);
-            }
-        });
-    }
-
-    private void selectAccountForTransfer(InputManager m) {
-        Account[] accounts = getTransferAccounts(this.client);
-
-        if (accounts.length == 0) {
-            m.setPanel(new AlertMessageForm("No accounts to transfer from!") {
-                @Override
-                public void onOK() {
-                    handleInput(m);
-                }
-            });
-        } else {
-            m.setPanel(new SearchForm("Select account to transfer from:", new OptionsForm<Account>(accounts, ""){
-                @Override
-                public void onSelection(Account account) {
-                    handleTransfer(m , account);
-                }
-            }.getMainPanel()) {
-                @Override
-                public void onCancel() {
-                    handleInput(m);
-                }
-            });
-        }
-    }
-
-    private Account[] getTransferAccounts(Client client) {
-        List<Account> listOfTransferAccounts = new ArrayList<>();
-        for (Account account: client.getClientsAccounts()) {
-            if (account.canTransferOut(0)) {
-                listOfTransferAccounts.add(account);
-            }
-        }
-        Account[] arrayOfTransferAccounts = new Account[listOfTransferAccounts.size()];
-        listOfTransferAccounts.toArray(arrayOfTransferAccounts);
-        return arrayOfTransferAccounts;
-    }
-
-    private void handleTransfer(InputManager m, Account account) {
-        String[] attributes = {"Username of recipient", "Amount to transfer"};
-        m.setPanel(new TextInputForm("Transfer money", attributes) {
-            @Override
-            public void onCancel() {
-                handleInput(m);
-            }
-
-            @Override
-            public void onOk(String[] strings) {
-                String outputString = "";
-                boolean transferComplete = false;
-                Client client = null;
-                double amount = 0;
-
-                try {
-                    client = (Client) BankMachine.USER_MANAGER.get(strings[0]);
-                } catch (NullPointerException e) {
-                    outputString = "Invalid input for username!";
-                }
-
-                if (client == null) {
-                    if (outputString.equals("")) {
-                        outputString = "User with that username not found";
-                    }
-                } else if (client.getPrimaryAccount() == null) {
-                    outputString = "The selected client does not have an account you can transfer to!";
-                } else {
-                    try {
-                        amount = Double.parseDouble(strings[1]);
-                        amount = ((double)Math.round(amount*100))/100.0;
-                        try {
-                            account.transferOut(client.getPrimaryAccount(), amount);
-                            transferComplete = true;
-                            outputString = "Transferred successfully!";
-                        } catch (BankMachineException e) {
-                            outputString = e.toString();
-                        }
-                    } catch (NumberFormatException | NullPointerException e) {
-                        outputString = "Invalid amount!";
-                    }
-                }
-
-                if (transferComplete) {
-                    BankMachine.transFactory.newTransaction(amount, account, client.getPrimaryAccount(), LocalDateTime.now(), TransactionType.TRANSFER);
-                    m.setPanel(new AlertMessageForm(outputString) {
-                        @Override
-                        public void onOK() {
-                            handleInput(m);
-                        }
-                    });
-                } else {
-                    m.setPanel(new AlertMessageForm(outputString) {
-                        @Override
-                        public void onOK() {
-                            handleTransfer(m, account);
-                        }
-                    });
-                }
-            }
-        });
-    }
-
-    private void handleInternalTransfer(InputManager m) {
-        List<Account> accountList = client.getClientsAccounts();
-        if (accountList.size() < 2) {
-            m.setPanel(new AlertMessageForm("No account(s) to transfer between!") {
-                @Override
-                public void onOK() {
-                    handleInput(m);
-                }
-            });
-        } else {
-            m.setPanel(new InternalTransferForm(accountList) {
-                @Override
-                public void onOk(Account fromAccount, Account toAccount, String amountString) {
-                    String outputString;
-                    double amount = 0;
-                    boolean transferComplete = false;
-                    if (fromAccount.getID() == toAccount.getID()) {
-                        outputString = "Cannot transfer to same account!";
-                    } else {
-                        try {
-                            amount = Double.parseDouble(amountString);
-                            fromAccount.transferOut(toAccount, amount);
-                            outputString = "Transferred Successfully!";
-                            transferComplete = true;
-                        } catch (NumberFormatException e) {
-                            outputString = "Invalid amount!";
-                        } catch (BankMachineException e2) {
-                            outputString = e2.toString();
-                        }
-                    }
-                    if (transferComplete) {
-                        BankMachine.transFactory.newTransaction(amount, fromAccount, toAccount, LocalDateTime.now(), TransactionType.TRANSFER);
-                        m.setPanel(new AlertMessageForm(outputString) {
-                            @Override
-                            public void onOK() {
-                                handleInput(m);
-                            }
-                        });
-                    } else {
-                        m.setPanel(new AlertMessageForm(outputString) {
-                            @Override
-                            public void onOK() {
-                                handleInternalTransfer(m);
-                            }
-                        });
-                    }
-                }
-
-                @Override
-                public void onCancel() {
-                    handleInput(m);
-                }
-            });
-        }
-    }
-
-    private void transferMenu(InputManager m) {
-        String[] options = {"Internal Transfer", "Transfer to other user"};
-        m.setPanel(new SearchForm("What kind of transfer?", new OptionsForm<String>(options, "") {
-            @Override
-            public void onSelection(String s) {
-                switch (s) {
-                    case "Internal Transfer":
-                        handleInternalTransfer(m);
-                        return;
-                    case "Transfer to other user":
-                        selectAccountForTransfer(m);
-                }
-            }
-        }.getMainPanel()) {
-            @Override
-            public void onCancel() {
-                handleInput(m);
-            }
-        });
-    }
 
     private void handleWithdraw(InputManager m) {
-        Account[] accounts = getTransferAccounts(this.client);
+        Account[] accounts = new TransferGUIHandler(this, this.client).getTransferAccounts(this.client);
         m.setPanel(new SearchForm("Select account to withdraw from", new OptionsForm<Account>(accounts, "") {
             @Override
             public void onSelection(Account account) {
@@ -366,7 +138,7 @@ public class PersonalGUI implements Inputtable {
         }.getMainPanel()) {
             @Override
             public void onCancel() {
-
+                handleInput(m);
             }
         });
     }
@@ -435,7 +207,7 @@ public class PersonalGUI implements Inputtable {
                 newAccountCreationInput(m);
                 return;
             case "Transfer":
-                transferMenu(m);
+                new TransferGUIHandler(this, this.client).transferMenu(m);
                 return;
             case "Withdraw":
                 handleWithdraw(m);
@@ -447,24 +219,15 @@ public class PersonalGUI implements Inputtable {
                 new FinanceGUIHandler(this).handleFinance(m);
                 return;
             case "Add User To Account":
+                handleAddUser(m);
                 return;
             case "Update Profile":
                 new UpdateProfileGUI(client, this).handleInput(m);
-                return;
-            case "Accounts":
-                inputGetAccounts(m);
                 return;
             default:
                 break;
         }
         handleInput(m);
-    }
-
-    private void handleAccount(InputManager m, Account account) {
-        if(account==null){
-            return;
-        }
-        new AccountGUI(account).handleInput(m);
     }
 
     @Override
---
+++ phase2/src/bankmachine/gui/TransferGUIHandler.java
@@ -0,0 +1,200 @@
+package bankmachine.gui;
+
+import bankmachine.BankMachine;
+import bankmachine.account.Account;
+import bankmachine.exception.BankMachineException;
+import bankmachine.transaction.TransactionType;
+import bankmachine.users.Client;
+
+import javax.swing.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransferGUIHandler {
+    private PersonalGUI gui;
+    private Client client;
+
+    public TransferGUIHandler(PersonalGUI gui, Client client) {
+        this.gui = gui;
+        this.client = client;
+    }
+
+    private void selectAccountForTransfer(InputManager m) {
+        Account[] accounts = getTransferAccounts(this.client);
+
+        if (accounts.length == 0) {
+            m.setPanel(new AlertMessageForm("No accounts to transfer from!") {
+                @Override
+                public void onOK() {
+                    gui.handleInput(m);
+                }
+            });
+        } else {
+            m.setPanel(new SearchForm("Select account to transfer from:", new OptionsForm<Account>(accounts, ""){
+                @Override
+                public void onSelection(Account account) {
+                    handleTransfer(m , account);
+                }
+            }.getMainPanel()) {
+                @Override
+                public void onCancel() {
+                    gui.handleInput(m);
+                }
+            });
+        }
+    }
+
+    public Account[] getTransferAccounts(Client client) {
+        List<Account> listOfTransferAccounts = new ArrayList<>();
+        for (Account account: client.getClientsAccounts()) {
+            if (account.canTransferOut(0)) {
+                listOfTransferAccounts.add(account);
+            }
+        }
+        Account[] arrayOfTransferAccounts = new Account[listOfTransferAccounts.size()];
+        listOfTransferAccounts.toArray(arrayOfTransferAccounts);
+        return arrayOfTransferAccounts;
+    }
+
+    private void handleTransfer(InputManager m, Account account) {
+        String[] attributes = {"Username of recipient", "Amount to transfer"};
+        m.setPanel(new TextInputForm("Transfer money", attributes) {
+            @Override
+            public void onCancel() {
+                gui.handleInput(m);
+            }
+
+            @Override
+            public void onOk(String[] strings) {
+                String outputString = "";
+                boolean transferComplete = false;
+                Client client = null;
+                double amount = 0;
+
+                try {
+                    client = (Client) BankMachine.USER_MANAGER.get(strings[0]);
+                } catch (NullPointerException e) {
+                    outputString = "Invalid input for username!";
+                }
+
+                if (client == null) {
+                    if (outputString.equals("")) {
+                        outputString = "User with that username not found";
+                    }
+                } else if (client.getPrimaryAccount() == null) {
+                    outputString = "The selected client does not have an account you can transfer to!";
+                } else {
+                    try {
+                        amount = Double.parseDouble(strings[1]);
+                        amount = ((double)Math.round(amount*100))/100.0;
+                        try {
+                            account.transferOut(client.getPrimaryAccount(), amount);
+                            transferComplete = true;
+                            outputString = "Transferred successfully!";
+                        } catch (BankMachineException e) {
+                            outputString = e.toString();
+                        }
+                    } catch (NumberFormatException | NullPointerException e) {
+                        outputString = "Invalid amount!";
+                    }
+                }
+
+                if (transferComplete) {
+                    BankMachine.transFactory.newTransaction(amount, account, client.getPrimaryAccount(), LocalDateTime.now(), TransactionType.TRANSFER);
+                    m.setPanel(new AlertMessageForm(outputString) {
+                        @Override
+                        public void onOK() {
+                            gui.handleInput(m);
+                        }
+                    });
+                } else {
+                    m.setPanel(new AlertMessageForm(outputString) {
+                        @Override
+                        public void onOK() {
+                            handleTransfer(m, account);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void handleInternalTransfer(InputManager m) {
+        List<Account> accountList = client.getClientsAccounts();
+        if (accountList.size() < 2) {
+            m.setPanel(new AlertMessageForm("No account(s) to transfer between!") {
+                @Override
+                public void onOK() {
+                    gui.handleInput(m);
+                }
+            });
+        } else {
+            m.setPanel(new InternalTransferForm(accountList) {
+                @Override
+                public void onOk(Account fromAccount, Account toAccount, String amountString) {
+                    String outputString;
+                    double amount = 0;
+                    boolean transferComplete = false;
+                    if (fromAccount.getID() == toAccount.getID()) {
+                        outputString = "Cannot transfer to same account!";
+                    } else {
+                        try {
+                            amount = Double.parseDouble(amountString);
+                            fromAccount.transferOut(toAccount, amount);
+                            outputString = "Transferred Successfully!";
+                            transferComplete = true;
+                        } catch (NumberFormatException e) {
+                            outputString = "Invalid amount!";
+                        } catch (BankMachineException e2) {
+                            outputString = e2.toString();
+                        }
+                    }
+                    if (transferComplete) {
+                        BankMachine.transFactory.newTransaction(amount, fromAccount, toAccount, LocalDateTime.now(), TransactionType.TRANSFER);
+                        m.setPanel(new AlertMessageForm(outputString) {
+                            @Override
+                            public void onOK() {
+                                gui.handleInput(m);
+                            }
+                        });
+                    } else {
+                        m.setPanel(new AlertMessageForm(outputString) {
+                            @Override
+                            public void onOK() {
+                                handleInternalTransfer(m);
+                            }
+                        });
+                    }
+                }
+
+                @Override
+                public void onCancel() {
+                    gui.handleInput(m);
+                }
+            });
+        }
+    }
+
+    public void transferMenu(InputManager m) {
+        String[] options = {"Internal Transfer", "Transfer to other user"};
+        m.setPanel(new SearchForm("What kind of transfer?", new OptionsForm<String>(options, "") {
+            @Override
+            public void onSelection(String s) {
+                switch (s) {
+                    case "Internal Transfer":
+                        handleInternalTransfer(m);
+                        return;
+                    case "Transfer to other user":
+                        selectAccountForTransfer(m);
+                }
+            }
+        }.getMainPanel()) {
+            @Override
+            public void onCancel() {
+                gui.handleInput(m);
+            }
+        });
+    }
+
+}
