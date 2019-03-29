68269ecf4405eaaf93beb351182f87b493cf1298
Advaya24 <advaya24@yahoo.co.in>
2019-03-29 16:10:51 -0400

    Extracted WithdrawGUIHandler

--- phase2/src/bankmachine/gui/PersonalGUI.java
+++ phase2/src/bankmachine/gui/PersonalGUI.java
@@ -48,69 +48,6 @@ public class PersonalGUI implements Inputtable {
     }
 
 
-
-    private void handleWithdraw(InputManager m) {
-        Account[] accounts = new TransferGUIHandler(this, this.client).getTransferAccounts(this.client);
-        m.setPanel(new SearchForm("Select account to withdraw from", new OptionsForm<Account>(accounts, "") {
-            @Override
-            public void onSelection(Account account) {
-                handleWithdrawFor(account, m);
-            }
-        }.getMainPanel()) {
-            @Override
-            public void onCancel() {
-                handleInput(m);
-            }
-        });
-    }
-
-    private void handleWithdrawFor(Account account, InputManager m) {
-        String[] attributes = {"Enter withdrawal amount"};
-        m.setPanel(new TextInputForm("Withdraw Money", attributes) {
-            @Override
-            public void onCancel() {
-                handleWithdraw(m);
-            }
-
-            @Override
-            public void onOk(String[] strings) {
-                double amount;
-                try {
-                    amount = Double.parseDouble(strings[0]);
-                    account.withdraw(amount);
-                    BankMachine.transFactory.newTransaction(amount, account, null, LocalDateTime.now(), TransactionType.WITHDRAW);
-                    m.setPanel(new AlertMessageForm("Success! Please remember to collect your money") {
-                        @Override
-                        public void onOK() {
-                            handleInput(m);
-                        }
-                    });
-                } catch (NumberFormatException e) {
-                    m.setPanel(new AlertMessageForm("Invalid amount") {
-                        @Override
-                        public void onOK() {
-                            handleWithdrawFor(account, m);
-                        }
-                    });
-                } catch (NotEnoughMoneyException | NotEnoughBillsException | NegativeQuantityException e) {
-                    m.setPanel(new AlertMessageForm(e.toString()) {
-                        @Override
-                        public void onOK() {
-                            handleWithdrawFor(account, m);
-                        }
-                    });
-                } catch (BankMachineException e) {
-                    m.setPanel(new AlertMessageForm("Failed... Something went wrong!") {
-                        @Override
-                        public void onOK() {
-                            handleWithdrawFor(account, m);
-                        }
-                    });
-                }
-            }
-        });
-    }
-
     private void handleDeposit(InputManager m) {
         Account[] accounts = new Account[this.client.getClientsAccounts().size()];
         this.client.getClientsAccounts().toArray(accounts);
@@ -210,7 +147,7 @@ public class PersonalGUI implements Inputtable {
                 new TransferGUIHandler(this, this.client).transferMenu(m);
                 return;
             case "Withdraw":
-                handleWithdraw(m);
+                new WithdrawGUIHandler(this, this.client).handleWithdraw(m);
                 return;
             case "Deposit":
                 handleDeposit(m);
---
+++ phase2/src/bankmachine/gui/WithdrawGUIHandler.java
@@ -0,0 +1,84 @@
+package bankmachine.gui;
+
+import bankmachine.BankMachine;
+import bankmachine.account.Account;
+import bankmachine.exception.BankMachineException;
+import bankmachine.exception.NegativeQuantityException;
+import bankmachine.exception.NotEnoughBillsException;
+import bankmachine.exception.NotEnoughMoneyException;
+import bankmachine.transaction.TransactionType;
+import bankmachine.users.Client;
+
+import java.time.LocalDateTime;
+
+public class WithdrawGUIHandler {
+    private PersonalGUI gui;
+    private Client client;
+
+    public WithdrawGUIHandler(PersonalGUI gui, Client client){
+        this.gui = gui;
+        this.client = client;
+    }
+
+    void handleWithdraw(InputManager m) {
+        Account[] accounts = new TransferGUIHandler(this.gui, this.client).getTransferAccounts(this.client);
+        m.setPanel(new SearchForm("Select account to withdraw from", new OptionsForm<Account>(accounts, "") {
+            @Override
+            public void onSelection(Account account) {
+                handleWithdrawFor(account, m);
+            }
+        }.getMainPanel()) {
+            @Override
+            public void onCancel() {
+                gui.handleInput(m);
+            }
+        });
+    }
+
+    private void handleWithdrawFor(Account account, InputManager m) {
+        String[] attributes = {"Enter withdrawal amount"};
+        m.setPanel(new TextInputForm("Withdraw Money", attributes) {
+            @Override
+            public void onCancel() {
+                handleWithdraw(m);
+            }
+
+            @Override
+            public void onOk(String[] strings) {
+                double amount;
+                try {
+                    amount = Double.parseDouble(strings[0]);
+                    account.withdraw(amount);
+                    BankMachine.transFactory.newTransaction(amount, account, null, LocalDateTime.now(), TransactionType.WITHDRAW);
+                    m.setPanel(new AlertMessageForm("Success! Please remember to collect your money") {
+                        @Override
+                        public void onOK() {
+                            gui.handleInput(m);
+                        }
+                    });
+                } catch (NumberFormatException e) {
+                    m.setPanel(new AlertMessageForm("Invalid amount") {
+                        @Override
+                        public void onOK() {
+                            handleWithdrawFor(account, m);
+                        }
+                    });
+                } catch (NotEnoughMoneyException | NotEnoughBillsException | NegativeQuantityException e) {
+                    m.setPanel(new AlertMessageForm(e.toString()) {
+                        @Override
+                        public void onOK() {
+                            handleWithdrawFor(account, m);
+                        }
+                    });
+                } catch (BankMachineException e) {
+                    m.setPanel(new AlertMessageForm("Failed... Something went wrong!") {
+                        @Override
+                        public void onOK() {
+                            handleWithdrawFor(account, m);
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
