21def4ed6ba8780dccb167c071344d4cf30052df
Advaya24 <advaya24@yahoo.co.in>
2019-03-31 17:09:00 -0400

    Deleted legacy methods and added documentation part 3

--- phase2/src/bankmachine/gui/InputManager.java
+++ phase2/src/bankmachine/gui/InputManager.java
@@ -34,108 +34,43 @@ public class InputManager extends JFrame {
             "^\\d+(.\\d\\d?)?$"
     );
 
-    private String getValue(Pattern p, String name) {
-        while (true) {
-            String email = getInput("Please enter a(n) " + name + ": ");
-            if (p.matcher(email).matches()) {
-                return email;
-            }
-            System.out.println("Invalid " + name);
-        }
-    }
-
-    public String getEmail() {
-        return getValue(emailRe, "email");
-    }
-
-    public String getPhone() {
-        return getValue(phoneRe, "phone number");
-    }
-
-    public double getMoney() {
-        while (true) {
-            String s = getValue(doubleRe, "amount");
-            try {
-                return Double.parseDouble(s);
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid amount");
-            }
-        }
-    }
-
-    public int getInteger(String prompt) {
-        while (true) {
-            int i;
-            try {
-                i = Integer.parseInt(getInput(prompt));
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid integer");
-                continue;
-            }
-            if (i < 0) {
-                System.out.println("Positive numbers only please");
-            } else {
-                return i;
-            }
-        }
-    }
-
+    /**
+     * Matches email to regex and throws exception if it doesn't match
+     * @param email the string to match
+     * @throws InvalidEmailException the email was in invalid format
+     */
     public void checkEmail(String email) throws InvalidEmailException {
         if (!emailRe.matcher(email).matches()) {
             throw new InvalidEmailException();
         }
     }
 
+    /**
+     * Matches phone to regex and throws exception if it doesn't match
+     * @param phone the string to match
+     * @throws InvalidPhoneNumberException the phone was in invalid format
+     */
     public void checkPhone(String phone) throws InvalidPhoneNumberException {
         if (!phoneRe.matcher(phone).matches()) {
             throw new InvalidPhoneNumberException();
         }
     }
 
-    private Scanner input;
 
     public static void main(String[] args) {
         new InputManager().mainLoop();
     }
 
     public InputManager() {
-        input = new Scanner(System.in);
         this.setSize(new Dimension(800, 800));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    // Prompts for input
-    public String getInput(String s) {
-        System.out.print(s);
-        return input.nextLine();
-    }
 
-    private String getInput(Object[] objects) {
-        printObjects(objects);
-        return input.nextLine();
-    }
-
-    private void printObjects(Object[] objects) {
-        for (Object o : objects) {
-            System.out.print(o);
-        }
-    }
-
-    public LocalDateTime getDate() {
-
-        while (true) {
-            int year = getInteger("Enter the year: ");
-            int month = getInteger("Enter the month (1-12): ");
-            int day = getInteger("Enter the day (1-31): ");
-            try {
-                return LocalDateTime.of(year, month, day, 0, 0);
-            } catch (DateTimeException e) {
-                System.out.println("Invalid date");
-            }
-        }
-    }
-
-
+    /**
+     * Sets panel for JFrame
+     * @param form the form whose main panel is to be the current panel
+     */
     public void setPanel(Form form) {
         this.getContentPane().removeAll();
         this.repaint();
@@ -155,10 +90,4 @@ public class InputManager extends JFrame {
         this.dispose();
         System.exit(0);
     }
-
-
-    // Following method kept for future use:
-//    private void PerformTransaction(Account a) {
-//        // call methods from the specific account
-//    }
 }
