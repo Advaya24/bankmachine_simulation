3eacbfa0121720ee741f645195f387e38fa2fa82
meilechr <christopher.meile@mail.utoronto.ca>
2019-04-01 12:01:08 -0400

    Documented custom finance exception

--- phase2/src/bankmachine/finance/FinanceException.java
+++ phase2/src/bankmachine/finance/FinanceException.java
@@ -1,5 +1,10 @@
 package bankmachine.finance;
 
+/***
+ * Custom Exception Created for Finance Package: Used in GUI to catch any form of issue regarding invalid stocks, or
+ * exchanges.
+ */
+
 public class FinanceException extends Exception {
 
     public FinanceException(String message) {
